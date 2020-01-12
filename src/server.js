fc6edec80f35b7cbdc431c2a543c980ca0c3431c
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2020-01-12 01:12:59 -0300

    FIX: Fix in return token

--- src/server.js
+++ src/server.js
@@ -7,13 +7,17 @@ const server = express();
 server.get('/get-user/:user', async (req, res) => {
     var client = await credentials.getCredentials();
 
-    var user = await client.getUser(req.params.user).then(function(data) {
-        return data.body;
-    }, function(err) {
-        return res.json(err);
-    });
+    if (client.statusCode != 400) {
+        var user = await client.getUser(req.params.user).then(function(data) {
+            return data.body;
+        }, function(err) {
+            return res.json(err);
+        });
 
-    return res.json(user);
+        return res.json(user);
+    }
+
+    return res.json(client);
 });
 
 server.listen(3000);
