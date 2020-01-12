0c71c51304b5ff0749908523527c359f1089e464
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2020-01-12 00:33:34 -0300

    NEW: Get informations by user

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const credentials = require('./services/credentials')
 
 const server = express();
+
+
+server.get('/get-user/:user', async (req, res) => {
+    var client = await credentials.getCredentials();
+
+    var user = await client.getUser(req.params.user).then(function(data) {
+        return data.body;
+    }, function(err) {
+        return res.json(err);
+    });
+
+    return res.json(user);
+});
 
 server.listen(3000);
